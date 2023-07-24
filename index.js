a164a0f68eb24302ecb6df30a6f81f52e21536f9
Balaji <97956046+Balaji340@users.noreply.github.com>
2023-07-24 20:17:21 +0530

    Update index.js
    update feature add button

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+function calculateTotal() {
+    let saveCounts = saveEl.textContent.split(" - ");
+    let total = 0;
+    for (let i = 0; i < saveCounts.length; i++) {
+        let countNum = parseInt(saveCounts[i]);
+        if (!isNaN(countNum)) {
+            total += countNum;
+        }
+    }
+    alert("Total: " + total);
 }
